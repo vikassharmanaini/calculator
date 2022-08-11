36ab98a913bf8f7717d10c6591ebb96cd0566b2a
Vikas <vikassharmanaini@gmail.com>
2022-08-11 13:52:57 +0530

    first version created

---
+++ lib/buttons.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final color;
+  final textColor;
+  final String buttonText;
+  final buttontapped;
+  MyButton({this.color, this.textColor, required this.buttonText, this.buttontapped});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttontapped,
+      child: Padding(
+        padding: const EdgeInsets.all(0.2),
+        child: Padding(
+          padding: const EdgeInsets.all(0.2),
+          child: ClipRRect(
+              child: Container(
+            color: color,
+            child: Center(
+                child: Text(
+              buttonText,
+              style: TextStyle(
+                color: textColor,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+          )),
+        ),
+      ),
+    );
+  }
+}
